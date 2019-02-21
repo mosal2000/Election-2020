88cb1683c6eed206c1647ba54bbbd43a50b65aab
mosal2000 <mohammad.ronosentono@gmail.com>
2019-02-21 15:42:05 -0500

    fix Data

---
+++ congressional_district_csv_merging_notebooks/election_prediction_db3.sql
@@ -0,0 +1,7 @@
+SELECT id  FROM election_prediction_db.district_demographic
+where SUBSTRING(state_dist, 1, 2) in ('AK', 'DE', 'MT', 'ND', 'SD', 'VT', 'WY')
+;
+
+update election_prediction_db.district_demographic
+set state_dist = concat(SUBSTRING(state_dist, 1, 4), '0')
+where id in (8, 87, 233, 298, 352, 402, 435);
